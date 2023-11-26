ec1a9f8046c6b21c04c36efa79e66a496e18036a
Sergey Dvoynikov <s@dvoyni.com>
2023-11-26 11:02:00 +0100

    Program.script implemented

--- native/js/index.js
+++ native/js/index.js
@@ -1,7 +1,8 @@
 export default function (runtime) {
-    runtime.register("Oak.Core.Program", {
-        "script": function(fn) {
-            return null;
+    runtime.register("Oak.Program", {
+        "script": function (fn) {
+            runtime.executeFn(fn, [runtime.list([])]);
+            return {};
         }
     });
 }
